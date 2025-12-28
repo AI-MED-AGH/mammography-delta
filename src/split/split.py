cf48d272a801419be1ebf6035324883c1667144b
Konrad Zatorski <kodiz2005@gmail.com>
2025-12-28 19:16:34 +0100

    Implement Patient-Aware Train/Test Split - Fixes #9. - Implement GroupShuffleSplit based aware_patient_split function. - The training and testing sets contain mutually exclusive lists of patients. - The ratio of Benign/Malignant cases remains preserved. - Intersection of patients IDs in Train and Test sets is empty.

---
+++ src/split/split.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit
+
+RANDOM_STATE = 49  # Random State was selected to maintain balanced y mean in train and test sets
+TEST_SIZE = 0.2
+
+
+def aware_patient_split(features_vector, drop_cols) -> tuple:
+    """
+    Split data into train and test sets that is aware of patient's id.
+    Rows with the same patient id only go to train or test set - never to both at the same time.
+
+    Args:
+        features_vector (pd.DataFrame): A DataFrame containing the features vector.
+        drop_cols: (list): A list of columns that will be dropped.
+    Returns:
+       X_train, X_test, y_train, y_test, is_intersect_empty (tuple):
+       Train and test DataFrame sets prepared for model training and True/False aware split test variable.
+    """
+
+    groups = features_vector['patient_id'].values
+    gss = GroupShuffleSplit(n_splits=1,
+                            test_size=TEST_SIZE,
+                            random_state=RANDOM_STATE)
+
+    train_idx, test_idx = next(gss.split(features_vector, features_vector['pathology'], groups=groups))
+
+    df_train = features_vector.iloc[train_idx]
+    df_test = features_vector.iloc[test_idx]
+
+    features_cols = [c for c in features_vector.columns if c not in drop_cols]
+
+    X_train = df_train[features_cols]
+    y_train = df_train['pathology']
+
+    X_test = df_test[features_cols]
+    y_test = df_test['pathology']
+
+    df_train.to_csv('train_split.csv', index=False)
+    df_test.to_csv('test_split.csv', index=False)
+
+    is_intersect_empty = set(train_idx).isdisjoint(set(test_idx))
+
+    return X_train, X_test, y_train, y_test, is_intersect_empty
+
+
+# print(f"Train features: {X_train.shape}")
+# print(f"Test features:    {X_test.shape}")
+
+# print("y mean in train:", y_train.mean())
+# print("y mean in test: ", y_test.mean())
+
+"""
+Train features: (1323, 28)
+Test features:    (341, 28)
+
+y mean in train: 0.4580498866213152
+y mean in test:  0.4604105571847507
+
+FROM DATA_ANALYSIS:
+- BENIGN:     901 - 54.15%
+- MALIGNANT:  763 - 45.85%
+
+CONCLUSION:
+LABELS DISTRIBUTION BETWEEN TRAIN AND TEST SETS IS 
+PRESERVED AT APPROX. 45% OF MALIGNANT IN EACH SET
+"""
